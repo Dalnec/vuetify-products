eeebcf60ba309ea41565536cfc626aecc3ca60b7
Dalnec <dalnec14@gmail.com>
2023-09-22 19:05:49 -0500

    init login

---
+++ src/views/Login.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-sheet
+    class="h-screen d-flex align-center justify-center mb-6"
+    height="100%"
+  >
+    <div>
+      <v-card class="pa-12" elevation="10" width="400" rounded="lg">
+        <v-img
+          class="mx-auto mb-6"
+          max-width="228"
+          src="https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-v3-slim-text-light.svg"
+        ></v-img>
+        <div class="text-subtitle-1 text-medium-emphasis">Account</div>
+
+        <v-text-field
+          density="compact"
+          placeholder="Email address"
+          prepend-inner-icon="mdi-email-outline"
+          variant="outlined"
+        ></v-text-field>
+
+        <div
+          class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between"
+        >
+          Password
+
+          <a
+            class="text-caption text-decoration-none text-blue"
+            href="#"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Forgot login password?</a
+          >
+        </div>
+
+        <v-text-field
+          :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+          :type="visible ? 'text' : 'password'"
+          density="compact"
+          placeholder="Enter your password"
+          prepend-inner-icon="mdi-lock-outline"
+          variant="outlined"
+          @click:append-inner="visible = !visible"
+        ></v-text-field>
+
+        <v-btn block class="mb-8" color="blue" size="large" variant="tonal">
+          Log In
+        </v-btn>
+
+        <v-card-text class="text-center">
+          <a
+            class="text-blue text-decoration-none"
+            href="#"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
+          </a>
+        </v-card-text>
+      </v-card>
+    </div>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    visible: false,
+  }),
+};
+</script>
